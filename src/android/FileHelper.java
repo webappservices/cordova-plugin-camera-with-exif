a5fa44c3e32c81e7017b240091f12c7d723ab4b9
Ionel Alexandru Dimitriu <alexandru@dimitriu.io>
2018-10-24 13:28:45 +0300

    Added support for SD Card

--- src/android/FileHelper.java
+++ src/android/FileHelper.java
@@ -16,12 +16,14 @@
  */
 package org.apache.cordova.camera;
 
+import android.util.Log;
 import android.annotation.SuppressLint;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.CursorLoader;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.OpenableColumns;
 import android.os.Build;
 import android.os.Environment;
 import android.provider.DocumentsContract;
@@ -36,7 +38,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Locale;
 
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.io.File;
+import java.util.Arrays;
+
+
 public class FileHelper {
+    private static final String TAG = "[FilePath plugin]: ";
     private static final String LOG_TAG = "FileUtils";
     private static final String _DATA = "_data";
 
@@ -45,20 +56,27 @@ public class FileHelper {
      * If the given URI string represents a content:// URI, the real path is retrieved from the media store.
      *
      * @param uriString the URI string of the audio/image/video
-     * @param cordova the current application context
+     * @param cordova   the current application context
      * @return the full path to the file
      */
     @SuppressWarnings("deprecation")
     public static String getRealPath(Uri uri, CordovaInterface cordova) {
         String realPath = null;
 
+
         if (Build.VERSION.SDK_INT < 11)
             realPath = FileHelper.getRealPathFromURI_BelowAPI11(cordova.getActivity(), uri);
 
-        // SDK >= 11
+            // SDK >= 11
         else
             realPath = FileHelper.getRealPathFromURI_API11_And_Above(cordova.getActivity(), uri);
 
+
+        //Return the URI string if no other type of path has been found
+        if (realPath == null)
+            realPath = uri.toString();
+
+        Log.d(TAG, "filepath de la final " + realPath);
         return realPath;
     }
 
@@ -66,7 +84,7 @@ public class FileHelper {
      * Returns the real path of the given URI.
      * If the given URI is a content:// URI, the real path is retrieved from the media store.
      *
-     * @param uri the URI of the audio/image/video
+     * @param uri     the URI of the audio/image/video
      * @param cordova the current application context
      * @return the full path to the file
      */
@@ -75,32 +93,48 @@ public class FileHelper {
     }
 
     @SuppressLint("NewApi")
-    public static String getRealPathFromURI_API11_And_Above(final Context context, final Uri uri) {
+
+    private static String getRealPathFromURI_API11_And_Above(final Context context, final Uri uri) {
+
+//        Log.d(TAG, "File - " +
+//                "Authority: " + uri.getAuthority() +
+//                ", Fragment: " + uri.getFragment() +
+//                ", Port: " + uri.getPort() +
+//                ", Query: " + uri.getQuery() +
+//                ", Scheme: " + uri.getScheme() +
+//                ", Host: " + uri.getHost() +
+//                ", Segments: " + uri.getPathSegments().toString()
+//        );
 
         final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
         // DocumentProvider
         if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-
             // ExternalStorageProvider
             if (isExternalStorageDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
                 final String type = split[0];
 
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
 
-                // TODO handle non-primary volumes
+                String fullPath = getPathFromExtSD(split);
+                if (fullPath != "") {
+                    return fullPath;
+                } else {
+                    return null;
+                }
             }
             // DownloadsProvider
             else if (isDownloadsDocument(uri)) {
-
                 final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+                try {
+                    final Uri contentUri = ContentUris.withAppendedId(
+                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
 
-                return getDataColumn(context, contentUri, null, null);
+                    return getDataColumn(context, contentUri, null, null);
+                } catch (NumberFormatException e) {
+                    //In Android 8 and Android P the id is not a number
+                    return uri.getPath().replaceFirst("^/document/raw:", "").replaceFirst("^raw:", "");
+                }
             }
             // MediaProvider
             else if (isMediaDocument(uri)) {
@@ -118,19 +152,36 @@ public class FileHelper {
                 }
 
                 final String selection = "_id=?";
-                final String[] selectionArgs = new String[] {
+                final String[] selectionArgs = new String[]{
                         split[1]
                 };
 
                 return getDataColumn(context, contentUri, selection, selectionArgs);
+            } else if (isGoogleDriveUri(uri)) {
+                return getDriveFilePath(uri, context);
             }
         }
         // MediaStore (and general)
         else if ("content".equalsIgnoreCase(uri.getScheme())) {
 
             // Return the remote address
-            if (isGooglePhotosUri(uri))
-                return uri.getLastPathSegment();
+            if (isGooglePhotosUri(uri)) {
+                String contentPath = getContentFromSegments(uri.getPathSegments());
+                Log.wtf(TAG, "contentPath " + contentPath);
+                if (contentPath != "") {
+                    Log.wtf(TAG, "contentPath cand e diferit de nimic ");
+                    return getRealPathFromURI_API11_And_Above(context, Uri.parse(contentPath));
+                } else {
+                    return null;
+                }
+            }
+
+            if (isGoogleDriveUri(uri)) {
+                Log.wtf(TAG, "isGoogleDriveUri(uri) " + getDriveFilePath(uri, context));
+                return getDriveFilePath(uri, context);
+            }
+
+            Log.wtf(TAG, "e la final");
 
             return getDataColumn(context, uri, null, null);
         }
@@ -142,8 +193,79 @@ public class FileHelper {
         return null;
     }
 
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Drive.
+     */
+    private static boolean isGoogleDriveUri(Uri uri) {
+        return "com.google.android.apps.docs.storage".equals(uri.getAuthority()) || "com.google.android.apps.docs.storage.legacy".equals(uri.getAuthority());
+    }
+
+
+    /**
+     * Get content:// from segment list
+     * In the new Uri Authority of Google Photos, the last segment is not the content:// anymore
+     * So let's iterate through all segments and find the content uri!
+     *
+     * @param segments The list of segment
+     */
+    private static String getContentFromSegments(List<String> segments) {
+        String contentPath = "";
+
+        for (String item : segments) {
+            if (item.startsWith("content://")) {
+                contentPath = item;
+                break;
+            }
+        }
+
+        return contentPath;
+    }
+
+
+    private static String getDriveFilePath(Uri uri, Context context) {
+        Uri returnUri = uri;
+        Cursor returnCursor = context.getContentResolver().query(returnUri, null, null, null, null);
+        /*
+         * Get the column indexes of the data in the Cursor,
+         *     * move to the first row in the Cursor, get the data,
+         *     * and display it.
+         * */
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+        returnCursor.moveToFirst();
+        String name = (returnCursor.getString(nameIndex));
+        String size = (Long.toString(returnCursor.getLong(sizeIndex)));
+        File file = new File(context.getCacheDir(), name);
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(uri);
+            FileOutputStream outputStream = new FileOutputStream(file);
+            int read = 0;
+            int maxBufferSize = 1 * 1024 * 1024;
+            int bytesAvailable = inputStream.available();
+
+            //int bufferSize = 1024;
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+
+            final byte[] buffers = new byte[bufferSize];
+            while ((read = inputStream.read(buffers)) != -1) {
+                outputStream.write(buffers, 0, read);
+            }
+            Log.e("File Size", "Size " + file.length());
+            inputStream.close();
+            outputStream.close();
+            Log.e("File Path", "Path " + file.getPath());
+            Log.e("File Size", "Size " + file.length());
+        } catch (Exception e) {
+            Log.e("Exception", e.getMessage());
+        }
+        return file.getPath();
+    }
+
+
     public static String getRealPathFromURI_BelowAPI11(Context context, Uri contentUri) {
-        String[] proj = { MediaStore.Images.Media.DATA };
+        String[] proj = {MediaStore.Images.Media.DATA};
         String result = null;
 
         try {
@@ -162,7 +284,7 @@ public class FileHelper {
      * Returns an input stream based on given URI string.
      *
      * @param uriString the URI string from which to obtain the input stream
-     * @param cordova the current application context
+     * @param cordova   the current application context
      * @return an input stream into the data at the given URI or null if given an invalid URI string
      * @throws IOException
      */
@@ -225,7 +347,7 @@ public class FileHelper {
         }
         return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
     }
-    
+
     /**
      * Returns the mime type of the data specified by the given URI string.
      *
@@ -249,9 +371,9 @@ public class FileHelper {
      * Get the value of the data column for this Uri. This is useful for
      * MediaStore Uris, and other file-based ContentProviders.
      *
-     * @param context The context.
-     * @param uri The Uri to query.
-     * @param selection (Optional) Filter used in the query.
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
      * @param selectionArgs (Optional) Selection arguments used in the query.
      * @return The value of the _data column, which is typically a file path.
      * @author paulburke
@@ -281,6 +403,7 @@ public class FileHelper {
         }
         return null;
     }
+
 
     /**
      * @param uri The Uri to check.
@@ -315,5 +438,66 @@ public class FileHelper {
      */
     public static boolean isGooglePhotosUri(Uri uri) {
         return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+
+    /**
+     * Check if a file exists on device
+     *
+     * @param filePath The absolute file path
+     */
+    private static boolean fileExists(String filePath) {
+        File file = new File(filePath);
+
+        return file.exists();
+    }
+
+    /**
+     * Get full file path from external storage
+     *
+     * @param pathData The storage type and the relative path
+     */
+    private static String getPathFromExtSD(String[] pathData) {
+        final String type = pathData[0];
+        final String relativePath = "/" + pathData[1];
+        String fullPath = "";
+
+
+        //Check directly on stroage and sd card name
+        fullPath = "/storage/" + type + "/" + relativePath;
+        if (fileExists(fullPath)) {
+            return fullPath;
+        }
+
+        // on my Sony devices (4.4.4 & 5.1.1), `type` is a dynamic string
+        // something like "71F8-2C0A", some kind of unique id per storage
+        // don't know any API that can get the root path of that storage based on its id.
+        //
+        // so no "primary" type, but let the check here for other devices
+        if ("primary".equalsIgnoreCase(type)) {
+
+            fullPath = Environment.getExternalStorageDirectory() + relativePath;
+            if (fileExists(fullPath)) {
+                return fullPath;
+            }
+        }
+
+        // Environment.isExternalStorageRemovable() is `true` for external and internal storage
+        // so we cannot relay on it.
+        //
+        // instead, for each possible path, check if file exists
+        // we'll start with secondary storage as this could be our (physically) removable sd card
+        fullPath = System.getenv("SECONDARY_STORAGE") + relativePath;
+        if (fileExists(fullPath)) {
+            return fullPath;
+        }
+
+
+        fullPath = System.getenv("EXTERNAL_STORAGE") + relativePath;
+        if (fileExists(fullPath)) {
+            return fullPath;
+        }
+        
+        return fullPath;
     }
 }
